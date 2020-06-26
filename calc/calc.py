8a33ab03610042c68513a2b45fc0bcd998d3185e
yangjiwoong1 <wpyang123@naver.com>
2020-06-26 21:39:58 +0900

    Issue #6 if --> try, except:

--- calc/calc.py
+++ calc/calc.py
@@ -7,10 +7,12 @@ def application(environ, start_response):
     first_num = d.get('first_num', [''])[0]
     second_num  = d.get('second_num', [''])[0]
     sum,mul='???','???' 
-    if '' not in [first_num,second_num]:
+    try:
         first_num, second_num = int(first_num), int(second_num)
         sum = first_num + second_num
-        mul = first_num * second_num       
+        mul = first_num * second_num
+    excpet ValueError:
+        pass       
     response_body = html%{'sum':sum, 'mul':mul}
     start_response('200 OK', [
         ('Content-Type', 'text/html'),
